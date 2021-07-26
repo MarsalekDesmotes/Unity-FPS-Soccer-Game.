439cdc41bbe96393434b6308da6ff3bfe362abd2
MarsalekDesmotes <fangednoumena12@gmail.com>
2021-07-26 03:03:19 +0300

    2.7
    hud eklendi

--- SoccerGame/Assets/ScoreHolderBlue.cs
+++ SoccerGame/Assets/ScoreHolderBlue.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScoreHolder : MonoBehaviour
+public class ScoreHolderBlue : MonoBehaviour
 {
-
-    Text scoretext;
+    // Start is called before the first frame update
+    Text scoretext2;
 
     // Start is called before the first frame update
 
     private void Awake()
     {
-        scoretext = GetComponent<Text>();
+        scoretext2 = GetComponent<Text>();
         GoalHolderBlue.mavitakimgol = 0;
         GoalHolderKirmizi.kýrmýzýtakýmgol = 0;
 
     }
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoretext.text = " " + GoalHolderKirmizi.kýrmýzýtakýmgol;
+        scoretext2.text = " " + GoalHolderBlue.mavitakimgol;
     }
 }
